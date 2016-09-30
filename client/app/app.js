0cf7544114cdad4640ffdb799678dc2186b72448
Joey Gauthier <Paul.Gauthier@tolling.telvent.com>
2016-09-30 09:02:56 -0500

    preemptively added some directive and started structure of SPA

--- client/app/app.js
+++ client/app/app.js
@@ -1,76 +1,113 @@
 'use strict';
 
 angular.module('vrlabApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngAnimate',
-  'ngMessages',
-  'ui.router',
-  'ngMaterial'
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngAnimate',
+    'ngMessages',
+    'ui.router',
+    'ngMaterial',
+    'angulike'
 ])
-  .config(function($mdIconProvider) {
+.config(function ($mdIconProvider) {
     $mdIconProvider
-      .iconSet('action', '../assets/iconsets/action-icons.svg', 24)
-      .iconSet('alert', '../assets/iconsets/alert-icons.svg', 24)
-      .iconSet('av', '../assets/iconsets/av-icons.svg', 24)
-      .iconSet('communication', '../assets/iconsets/communication-icons.svg', 24)
-      .iconSet('content', '../assets/iconsets/content-icons.svg', 24)
-      .iconSet('device', '../assets/iconsets/device-icons.svg', 24)
-      .iconSet('editor', '../assets/iconsets/editor-icons.svg', 24)
-      .iconSet('file', '../assets/iconsets/file-icons.svg', 24)
-      .iconSet('hardware', '../assets/iconsets/hardware-icons.svg', 24)
-      .iconSet('icons', '../assets/iconsets/icons-icons.svg', 24)
-      .iconSet('image', '../assets/iconsets/image-icons.svg', 24)
-      .iconSet('maps', '../assets/iconsets/maps-icons.svg', 24)
-      .iconSet('navigation', '../assets/iconsets/navigation-icons.svg', 24)
-      .iconSet('notification', '../assets/iconsets/notification-icons.svg', 24)
-      .iconSet('social', '../assets/iconsets/social-icons.svg', 24)
-      .iconSet('toggle', '../assets/iconsets/toggle-icons.svg', 24)
-      .iconSet('avatar', '../assets/iconsets/avatar-icons.svg', 128);
-  })
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+        .iconSet('action', '../assets/iconsets/action-icons.svg', 24)
+        .iconSet('alert', '../assets/iconsets/alert-icons.svg', 24)
+        .iconSet('av', '../assets/iconsets/av-icons.svg', 24)
+        .iconSet('communication', '../assets/iconsets/communication-icons.svg', 24)
+        .iconSet('content', '../assets/iconsets/content-icons.svg', 24)
+        .iconSet('device', '../assets/iconsets/device-icons.svg', 24)
+        .iconSet('editor', '../assets/iconsets/editor-icons.svg', 24)
+        .iconSet('file', '../assets/iconsets/file-icons.svg', 24)
+        .iconSet('hardware', '../assets/iconsets/hardware-icons.svg', 24)
+        .iconSet('icons', '../assets/iconsets/icons-icons.svg', 24)
+        .iconSet('image', '../assets/iconsets/image-icons.svg', 24)
+        .iconSet('maps', '../assets/iconsets/maps-icons.svg', 24)
+        .iconSet('navigation', '../assets/iconsets/navigation-icons.svg', 24)
+        .iconSet('notification', '../assets/iconsets/notification-icons.svg', 24)
+        .iconSet('social', '../assets/iconsets/social-icons.svg', 24)
+        .iconSet('toggle', '../assets/iconsets/toggle-icons.svg', 24)
+        .iconSet('avatar', '../assets/iconsets/avatar-icons.svg', 128);
+})
+.config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
-      .otherwise('/');
+        .otherwise('/');
 
     $locationProvider.html5Mode(true);
     $httpProvider.interceptors.push('authInterceptor');
-  })
-
-  .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
+})
+.factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
     return {
-      // Add authorization token to headers
-      request: function (config) {
-        config.headers = config.headers || {};
-        if ($cookieStore.get('token')) {
-          config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
-        }
-        return config;
-      },
+        // Add authorization token to headers
+        request: function (config) {
+            config.headers = config.headers || {};
+            if ($cookieStore.get('token')) {
+                config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
+            }
+            return config;
+        },
 
-      // Intercept 401s and redirect you to login
-      responseError: function(response) {
-        if(response.status === 401) {
-          $location.path('/login');
-          // remove any stale tokens
-          $cookieStore.remove('token');
-          return $q.reject(response);
+        // Intercept 401s and redirect you to login
+        responseError: function (response) {
+            if(response.status === 401) {
+                $location.path('/login');
+                // remove any stale tokens
+                $cookieStore.remove('token');
+                return $q.reject(response);
+            } else {
+                return $q.reject(response);
+            }
         }
-        else {
-          return $q.reject(response);
-        }
-      }
     };
-  })
-
-  .run(function ($rootScope, $location, Auth) {
+})
+.run(function ($rootScope, $location, Auth) {
     // Redirect to login if route requires auth and you're not logged in
     $rootScope.$on('$stateChangeStart', function (event, next) {
-      Auth.isLoggedInAsync(function(loggedIn) {
-        if (next.authenticate && !loggedIn) {
-          event.preventDefault();
-          $location.path('/login');
-        }
-      });
+        Auth.isLoggedInAsync(function (loggedIn) {
+            if (next.authenticate && !loggedIn) {
+                event.preventDefault();
+                $location.path('/login');
+            }
+        });
     });
-  });
+    // konami code
+    var neededkeys = [38,38,40,40,37,39,37,39,66,65],
+        started = false,
+        count = 0;
+    function reset() {
+        started = false; count = 0;
+       return;
+    }
+    $(document).keydown(function (e){
+        var key = e.keyCode;
+        //Set start to true only if having pressed the first key in the konami sequence.
+        if (!started){
+            if (key === 38){
+                started = true;
+            }
+        }
+        //If we've started, pay attention to key presses, looking for right sequence.
+        if (started){
+            if (neededkeys[count] === key){
+                //We're good so far.
+                count++;
+            } else {
+                //Oops, not the right sequence, lets restart from the top.
+                reset();
+            }
+            if (count === 10){
+                //We made it! Do something cool here.
+                console.log('konami easter egg');
+                // audio.play();
+                // $scope.konami = true;
+                //Reset the conditions so that someone can do it all again.
+                reset();
+            }
+        } else {
+            //Oops.
+            reset();
+        }
+    });
+       //Function used to reset us back to starting point.
+});
--- client/app/main.js
+++ client/app/main.js
@@ -7,5 +7,29 @@ angular.module('vrlabApp').config(function ($stateProvider) {
         	templateUrl: 'app/main/main.html',
         	controller: 'MainCtrl',
         	controllerAs: 'vm'
+      	})
+      	.state('main.home', {
+      		parent: 'main',
+      		templateUrl: 'app/home/home.html',
+      		controller: 'app/home/homeCtrl.js',
+      		controllerAs: 'hc'
+      	})
+      	.state('main.about', {
+      		parent: 'main',
+      		templateUrl: 'app/about/about.html',
+      		controller: 'app/about/aboutCtrl.js',
+      		controllerAs: 'ac'
+      	})
+      	.state('main.products', {
+      		parent: 'main',
+      		templateUrl: 'app/products/products.html',
+      		controller: 'app/products/productsCtrl.js',
+      		controllerAs: 'pc'
+      	})
+      	.state('main.contact', {
+      		parent: 'main',
+      		templateUrl: 'app/contact/contact.html',
+      		controller: 'app/contact/contactCtrl.js',
+      		controllerAs: 'cc'
       	});
   	});
